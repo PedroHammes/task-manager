741c1505a967b9723e039747610e021bdaa91108
pedrohammes <predohammes@gmail.com>
2025-07-15 14:56:43 -0300

    permancência de dados no BD concluída

--- src/database.js
+++ src/database.js
@@ -1,5 +1,19 @@
+import fs from "node:fs/promises"
+
+const databasePath = new URL('../db.json', import.meta.url)
+
 export class Database {
     #database = {}
+    
+    constructor() {
+        fs.readFile(databasePath, 'utf8')
+        .then(data => this.#database = JSON.parse(data))
+        .catch(() => this.#persist())
+    }
+
+    #persist() {
+        fs.writeFile(databasePath, JSON.stringify(this.#database))
+    }
 
     insert(table, data) {
         
@@ -11,6 +25,8 @@ export class Database {
         } else {
             this.#database[table] = [data]
         }
+
+        this.#persist()
     }
 
     select(table) {
